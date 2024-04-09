657226d065f71a8b04b3267429cd57b0b055ac06
1062076461 <1062076461@PC22SJ209.fup.edu>
2024-04-09 10:50:13 -0500

    V.2

--- src/main/java/Logica_Conexion/PersonaProvider.java
+++ src/main/java/Logica_Conexion/PersonaProvider.java
@@ -9,5 +9,21 @@ package Logica_Conexion;
  * @author perfil
  */
 public class PersonaProvider {
+    CollectionReference reference;
+    public static Firestore db;
+    
+    public static boolean GuardarPersona(String colection, String documento, Map<String, Object> data){
+        db = FirestoreClient.getFirestore();
+        try{
+            DocumentReference docRef = db.collection(colection).document(documento);
+            ApiFuture<WriteResult> result = docRef.set(data);
+            System.out.println("Guardado corectamente");
+            return true;
+            
+        } catch (Exception e){
+            System.out.println("Error:" + e.getMessage());
+        }
+        return false;
+    }
     
 }
