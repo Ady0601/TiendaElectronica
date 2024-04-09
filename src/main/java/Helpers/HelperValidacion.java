da0dcd4934f674c831c5ab07640f7fa3454f6a31
1002807593 <1002807593@PC28SJ209.fup.edu>
2024-04-09 11:27:53 -0500

    9nube Validaciones , correcion de Proucto

--- src/main/java/Helpers/HelperValidacion.java
+++ src/main/java/Helpers/HelperValidacion.java
@@ -12,29 +12,33 @@ import java.util.ArrayList;
  */
 public class HelperValidacion {
 
-    public static int RetornarValor(String Nombre) {
-        int numero = 0;
-        int letra = 0;
+      public static int RetornarValor(String nombre)
+    {
+        int numero=0;
+        int letra=0;
 
-        for (int j = 0; j < Nombre.length(); j++) {
-            boolean flag = Character.isDigit(Nombre.charAt(j));
-            if (flag) {
-                //System.out.println("'"+ Nombre.charAt(j)+"' is a number");
+        
+        for (int j = 0; j < nombre.length(); j++) {
+             boolean flag = Character.isDigit(nombre.charAt(j));
+             if(flag) {
+                //System.out.println("'"+ nombre.charAt(j)+"' is a number");
                 numero++;
-            } else {
-                //System.out.println("'"+ Nombre.charAt(j)+"' is a letter");
+             }
+             else {
+                //System.out.println("'"+ nombre.charAt(j)+"' is a letter");
                 letra++;
-            }
+             }
 
         }
         return numero;
     }
-
-    public static int RetornarCEV2(String Nombre) {
-        int ce = 0;
-
+     
+      public static int RetornarCEV2(String nombre)
+    {
+        int ce=0;
+        
         ArrayList<Character> lscaracteres = new ArrayList<>();
-
+        
         lscaracteres.add('@');
         lscaracteres.add('~');
         lscaracteres.add('/');
@@ -42,22 +46,24 @@ public class HelperValidacion {
         lscaracteres.add(':');
         lscaracteres.add('"');
         lscaracteres.add('!');
+        lscaracteres.add(' ');
 
-        for (int j = 0; j < Nombre.length(); j++) {
-            boolean flag = Character.isLetter(Nombre.charAt(j));
-            if (!flag) {
-                for (int i = 0; i < lscaracteres.size(); i++) {
-
-                    if (lscaracteres.get(i).compareTo(Nombre.charAt(j)) == 0) {
+        for (int j = 0; j < nombre.length(); j++) {
+             boolean flag = Character.isLetter(nombre.charAt(j));
+             if(!flag) {
+                 for (int i = 0; i < lscaracteres.size(); i++) {
+                      
+                     if(lscaracteres.get(i).compareTo(nombre.charAt(j))==0)
+                     {
                         ce++;
-                    }
-                }
-            }
+                     }
+                 }
+             }
         }
         return ce;
     }
-
-    public static int ValidarVacio(String cadena) {
+      
+      public static int ValidarVacio(String cadena) {
         if (cadena.equals("")) {
             return 1;
         } else {
@@ -65,68 +71,125 @@ public class HelperValidacion {
         }
 
     }
+      
+      
+       public static int RetornarLetra(String nombre)
+    {
+        int numero=0;
+        int letra=0;
 
-    public static int RetornarLetra(String Nombre) {
-        int numero = 0;
-        int letra = 0;
-
-        for (int j = 0; j < Nombre.length(); j++) {
-            boolean flag = Character.isDigit(Nombre.charAt(j));
-            if (flag) {
-                //System.out.println("'"+ Nombre.charAt(j)+"' is a number");
+        
+        for (int j = 0; j < nombre.length(); j++) {
+             boolean flag = Character.isDigit(nombre.charAt(j));
+             if(flag) {
+                //System.out.println("'"+ nombre.charAt(j)+"' is a number");
                 numero++;
-            } else {
-                //System.out.println("'"+ Nombre.charAt(j)+"' is a letter");
+             }
+             else {
+                //System.out.println("'"+ nombre.charAt(j)+"' is a letter");
                 letra++;
-            }
+             }
 
         }
         return letra;
     }
-
-    public static int ValidarCantidadRango(int cantidad) {
-        if (cantidad > 0 && cantidad < 1000) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    public static int ValidarTodo(String Cadena) {
-        int conteo = ValidarVacio(Cadena) + RetornarValor(Cadena) + RetornarCEV2(Cadena);
-        return conteo;
+       
+   public static int ValidarCantidadRango(int cantidad)
+   {
+       if(cantidad>0 && cantidad<1000){
+           return 1;
+       }
+      else
+       {
+           return 0;
+       }
+   }
+   
+     public static int RetornarCEDireccionV2(String nombre)
+    {
+        int ce=0;
         
-    }
-    public static int RetornarCEV2Contraseña(String Contraseña) {
-        int ce = 0;
-
         ArrayList<Character> lscaracteres = new ArrayList<>();
-
+        
         lscaracteres.add('@');
-        lscaracteres.add('~');
         lscaracteres.add('/');
         lscaracteres.add(';');
         lscaracteres.add(':');
         lscaracteres.add('"');
         lscaracteres.add('!');
 
-        for (int j = 0; j < Contraseña.length(); j++) {
-            boolean flag = Character.isLetter(Contraseña.charAt(j));
-            if (!flag) {
-                for (int i = 0; i < lscaracteres.size(); i++) {
-
-                    if (lscaracteres.get(i).compareTo(Contraseña.charAt(j)) == 0) {
+        for (int j = 0; j < nombre.length(); j++) {
+             boolean flag = Character.isLetter(nombre.charAt(j));
+             if(!flag) {
+                 for (int i = 0; i < lscaracteres.size(); i++) {
+                      
+                     if(lscaracteres.get(i).compareTo(nombre.charAt(j))==0)
+                     {
                         ce++;
-                    }
-                }
-            }
+                     }
+                 }
+             }
         }
         return ce;
     }
-    public static int ValidarTodoContraseña(String cadena)
-{
-int conteo=ValidarVacio(cadena)+RetornarCEV2Contraseña(cadena);
-return conteo;
-}
+     
+       public static int RetornarCEV2Contraseña(String nombre)
+    {
+        int ce=0;
+        
+        ArrayList<Character> lscaracteres = new ArrayList<>();
+        
+       
+     
+        lscaracteres.add('/');
+        lscaracteres.add(';');
+        lscaracteres.add(':');
+        lscaracteres.add('"');
+        lscaracteres.add('!');
+        lscaracteres.add(' ');
+
+        for (int j = 0; j < nombre.length(); j++) {
+             boolean flag = Character.isLetter(nombre.charAt(j));
+             if(!flag) {
+                 for (int i = 0; i < lscaracteres.size(); i++) {
+                      
+                     if(lscaracteres.get(i).compareTo(nombre.charAt(j))==0)
+                     {
+                        ce++;
+                     }
+                 }
+             }
+        }
+        return ce;
+    }
+     
+      public static int ValidarTodo(String cadena)
+   {
+       int conteo=ValidarVacio(cadena)+RetornarValor(cadena)+RetornarCEV2(cadena);
+       return conteo;
+   }
+   
+    public static int ValidarTodoLetra(String cadena)
+   {
+       int conteo=ValidarVacio(cadena)+RetornarLetra(cadena)+RetornarCEV2(cadena);
+       return conteo;
+   }
     
+    public static int ValidarTodoDireccion(String cadena)
+   {
+       int conteo=ValidarVacio(cadena)+RetornarCEDireccionV2(cadena);
+       return conteo;
+   }
+    
+      public static int ValidarTodoSerial(String cadena)
+   {
+       int conteo=ValidarVacio(cadena)+RetornarCEV2(cadena);
+       return conteo;
+   }
+      
+          public static int ValidarTodoContraseña(String cadena)
+   {
+       int conteo=ValidarVacio(cadena)+RetornarCEV2Contraseña(cadena);
+       return conteo;
+   }
 }
