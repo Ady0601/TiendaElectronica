81e99bf5815bad7aeb8b94a8955a4f9e8af365d7
1002807593 <1002807593@SJ209PC21.fup.edu>
2024-04-16 10:37:48 -0500

    17

--- src/main/java/Helpers/HelperImpresion.java
+++ src/main/java/Helpers/HelperImpresion.java
@@ -4,12 +4,54 @@
  */
 package Helpers;
 
+import Logica_Negocio.Persona;
+import Logica_Negocio.Producto;
+import java.util.ArrayList;
+
 /**
  *
  * @author perfil
  */
 public class HelperImpresion {
     
-    
+    public static void BuscarPersonaNube(ArrayList<Persona>lspersonasnube, String Codigo){
+        String[] parts = null,
+                parts2 = null;
+        int bandera = 0;
+        Producto objprodu;
+        ArrayList<Producto> lsnube = new ArrayList<>();
+        
+        for (int i = 0; i < lspersonasnube.size(); i++) {
+            if (Codigo.equals(lspersonasnube.get(i).getUid())){
+                bandera = 1;
+                System.out.println("Persona" + "\t" + (i + 1) + "\n"
+                        + "El id de la persona es:" + lspersonasnube.get(i).getUid() 
+                        + "El nombre de la persona es:" + "\t" + lspersonasnube.get(i).getNombre() + "\n"
+                        + "El apellido de la persona es:" + "\t" + lspersonasnube.get(i).getApellido() + "\n"
+                        + "La cedula de la persona es:" + "\t" + lspersonasnube.get(i).getCedula() + "\n"
+                        + "La direccion de la persona es:" + "\t" + lspersonasnube.get(i).getDireccion());
+                parts = lspersonasnube.get(i).getProducto().split("i");
+                
+                for (int j = 0; j < parts.length; j++){
+                    
+                    parts2 = parts[j].split(",");
+                    objprodu = new Producto(parts2[0],parts2[1],parts2[2]);
+                    lsnube.add(objprodu);
+                }
+                
+                for (int k  = 0; k < lsnube.size(); k++){
+                    System.out.println("Producto" + "\t" + (k + i));
+                    System.out.println("Nombre" + "\t" + lsnube.get(k).getNombre());
+                    System.out.println("Marca" + "\t" + lsnube.get(k).getMarca());
+                    System.out.println("Serial" + "\t" + lsnube.get(k).getSerial() + "\n");
+                }
+                lsnube.clear();
+                parts = null;
+                parts2 = null;
+            }
+            
+        }
+        
+    }
     
 }
